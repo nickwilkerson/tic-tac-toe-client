b6f407240591d0a5e3055311c34928996f85c422
nickwilkerson <nickwilkerson21@gmail.com>
2021-07-16 13:36:37 -0600

    made sign up field the default view on load up. sign up field loads on page start up, sign in field is hidden. on successful sign up, the sign in field appears, and the sign up field gets hidden. on successful sign in, sign up and sign in fields are hidden.

--- app/auth/ui.js
+++ app/auth/ui.js
@@ -1,18 +1,19 @@
-
+$('#sign-in-element').hide()
 const onSignUpSuccess = function (response) {
   $('#message').text(`Thanks For Signing Up ${response.user.email}!`)
   $('#sign-up').trigger('reset') // clears fields after sign up
+  $('#sign-in-element').show()
+  $('#sign-up-element').hide()
 }
 const onSignUpFailure = function () {
+  // $('#sign-in').hide()
   $('#message').text('Failed To Sign Up')
   $('#sign-up').trigger('reset') // clears fields after sign up
 }
 const onSignInSuccess = function (response) {
   $('#message').text(`Successfully Signed In ${response.user.email}!`)
   $('#sign-in').trigger('reset') // clears fields after sign in
-  // $('#sign-in').hide()
-  // $('#sign-up').hide()
-  // $('#sign-out').show()
+  $('#sign-in-element').hide()
 }
 const onSignInFailure = function () {
   $('#message').text('Failed To Sign In')
